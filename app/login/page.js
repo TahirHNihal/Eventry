a64c39ee654c29cbfba8864a74118c88cd13b7ad
TahirHNihal <tahirhnihal29@gmail.com>
2024-05-04 18:10:28 +0600

    updated bug

--- app/login/page.js
+++ app/login/page.js
@@ -9,7 +9,7 @@ const LoginPage = () => {
           <h4 className="font-bold text-2xl">Sign in</h4>
           <LoginForm />
           <span className="text-center text-xs text-gray-500">
-            Don't have an account?
+            Dont have an account?
             <Link
               className="underline hover:text-indigo-600 ml-1"
               href="/registration"
